98d75af72cafb48d51ea21eeb5cdf733a88b613b
trecho26 <hectorlara260698@gmail.com>
2021-03-07 11:43:09 -0700

    Configuracion de rutas y creacion de su modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -11,6 +13,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     PaisModule,
     SharedModule,
   ],
