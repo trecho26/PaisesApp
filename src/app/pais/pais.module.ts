08fbd5c48dfbcb42ce99578cff10549cef2cbf3f
trecho26 <hectorlara260698@gmail.com>
2021-03-07 11:25:24 -0700

    Correcion de pais.module

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -16,6 +16,12 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    PorCapitalComponent, 
+    PorPaisComponent, 
+    PorRegionComponent, 
+    VerPaisComponent
   ]
 })
 export class PaisModule { }
